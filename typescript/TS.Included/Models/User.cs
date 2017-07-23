17a7995939caad88dc34e0c89849ff4580e6c759
narekye <yegoryan.narek@gmail.com>
2017-07-22 21:51:31 -0600

    MVC delete

--- typescript/TS.Included/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TS.Included.Models
-{
-    public class User
-    {
-        public int Age { get; set; }
-        public string Name { get; set; }
-        public int Id { get; set; }
-    }
-}
