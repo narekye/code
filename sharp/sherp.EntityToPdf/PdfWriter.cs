2e6d5ab4d8cd285067195978bbaad49f4836c645
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-07-27 10:13:03 +0400

    Entity to pfd added

---
+++ sharp/sherp.EntityToPdf/PdfWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using itextsharp.pdfa;
+using iTextSharp.text.pdf;
+
+namespace sherp.EntityToPdf
+{
+    public static class PdfWriter
+    {
+        
+    }
+}
