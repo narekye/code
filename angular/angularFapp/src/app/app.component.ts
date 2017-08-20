97054ac60c739d4b6fac9ece1e2dcbb1d82a7d56
narekye <yegoryan.narek@gmail.com>
2017-08-19 21:29:32 -0600

    ts file optimize

--- angular/angularFapp/src/app/app.component.ts
+++ angular/angularFapp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {Http} from "@angular/http";
-import {User} from "./Models/Models";
+import {User} from "./Models/User";
 
 @Component({
   selector: 'app-root',
@@ -10,24 +10,13 @@ import {User} from "./Models/Models";
 export class AppComponent {
   // fields
   private connection: Http;
-
   constructor(http: Http) {
     this.connection = http;
     this.get();
   }
-  public flag: boolean = true;
   public users: User[] = [new User("a", "b"), new User("c", "d")];
-
-  // data: any;
-  // private lastname: any;
-
   get(): void {
-    // console.log("aaa");
     this.connection.get("http://crmbetd.azurewebsites.net/api/contacts").subscribe(data => console.log(data), error => console.log(error));
-    // let users: User[];
-    // this.users = ;
   }
-
-
   title = 'app';
 }
