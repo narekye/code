f0c52ad57a192630ca34b5ef160e9a0856edb4d5
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-09-07 18:14:15 +0400

    core commit of screen capturer

---
+++ sharp/utilities/sharp.ScreenCapturer.MailSender/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp.ScreenCapturer.MailSender
+{
+    public class Class1
+    {
+    }
+}
