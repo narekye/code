da2e37f6c6102ffd36cb1aa27fe985a4c55f82c5
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-09-11 15:54:13 +0400

    changes are made

---
+++ sharp/src/Utilities/sharp.ScreenCapturer/sharp.ScreenCapturer.Job/ScreenShootSaver.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace sharp.ScreenCapturer.Job
+{
+    public class ScreenShootSaver
+    {
+        public static void SaveImage(string path, Image img)
+        {
+            img.Save("temp.png", ImageFormat.Png);
+        }
+    }
+}
--- sharp/src/Utilities/sharp.ScreenCapturer/sharp.ScreenCapturer.Service/CapturerService.cs
+++ sharp/src/Utilities/sharp.ScreenCapturer/sharp.ScreenCapturer.Service/CapturerService.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace sharp.ScreenCapturer.Service
 {
     public partial class CapturerService : ServiceBase
     {
+
         public CapturerService()
         {
             InitializeComponent();
