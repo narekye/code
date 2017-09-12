b27d70183c377ada552f2e7b9802cf85fb378e66
Narek Yegoryan Home <yegoryan.narek@gmail.com>
2017-09-13 00:01:00 +0400

    extensions added

---
+++ sharp/src/Utilities/sharp.Extensions/Newtonsoft.Json/JsonExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace sharp.Extensions.Newtonsoft.Json
+{
+    public static class JsonExtensions
+    {
+        public static object ExpressionHelper { get; private set; }
+
+        public static string ToJson<T>(this T obj, bool includeNull = false)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new JsonConverter[] { new StringEnumConverter() },
+                NullValueHandling = includeNull ? NullValueHandling.Include : NullValueHandling.Ignore
+            };
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+    }
+}
