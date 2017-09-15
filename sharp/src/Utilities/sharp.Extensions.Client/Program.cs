66f05b03f00252bbe398bd946455e223a8bfe3de
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-09-15 12:31:27 +0400

    Qr code complete.

---
+++ sharp/src/Utilities/sharp.Extensions.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using sharp.Extensions.QR;
+using System;
+
+namespace sharp.Extensions.Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            QrCode.GenerateBarCode("Some text", 500, 500);
+            Console.Read();
+        }
+    }
+}
--- sharp/src/Utilities/sharp.Extensions/QR/QrCode.cs
+++ sharp/src/Utilities/sharp.Extensions/QR/QrCode.cs
@@ -1,7 +1,32 @@
-﻿namespace sharp.Extensions.QR
+﻿using System;
+using System.Drawing;
+using ZXing;
+using ZXing.QrCode;
+
+namespace sharp.Extensions.QR
 {
     public class QrCode
     {
-        public static void CreateBarCode(string encoding) { } //TODO: change return type
+        public static void GenerateBarCode(string text, int width = 250, int height = 250)
+        {
+            if (string.IsNullOrEmpty(text)) throw new ArgumentException("Text cannot be null or empty.");
+            var options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = width,
+                Height = height
+            };
+
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = options
+            };
+
+            var result = new Bitmap(writer.Write(text));
+            string date = System.DateTime.Now.ToShortDateString();
+            result.Save(date.Replace('/', '-') + ".jpeg");
+        }
     }
 }
