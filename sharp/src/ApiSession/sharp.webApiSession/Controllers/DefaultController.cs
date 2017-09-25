bde37287b71bfd3cb8323a43ccb7d8226a6b56bc
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-09-25 17:27:58 +0400

    commit

--- sharp/src/ApiSession/sharp.webApiSession/Controllers/DefaultController.cs
+++ sharp/src/ApiSession/sharp.webApiSession/Controllers/DefaultController.cs
@@ -1,5 +1,8 @@
-﻿using sharp.webApiSession.BLL.Repository;
+﻿using sharp.webApiSession.BLL.Models;
+using sharp.webApiSession.BLL.Repository;
 using sharp.webApiSession.Controllers.Core;
+using System;
+using System.Web;
 using System.Web.Http;
 
 namespace sharp.webApiSession.Controllers
@@ -7,13 +10,29 @@ namespace sharp.webApiSession.Controllers
 
     public class DefaultController : BaseApiController
     {
+        // its a bad practice to use session in REST APIs, but for testing stuff its all done))
         public DefaultController(IAdminRepository repository) : base(repository) { }
 
         [HttpGet]
         public IHttpActionResult DoWork()
         {
             var result = AdminRepository.DoWork();
-            return Ok(result);
+            var model = new SessionModel()
+            {
+                Token = Guid.NewGuid().ToString()
+            };
+
+            if (HttpContext.Current.Session["Some"] == null)
+            {
+                HttpContext.Current.Session["Some"] = model;
+            }
+
+            return Ok(HttpContext.Current.Session["Some"] as SessionModel);
+        }
+
+        public IHttpActionResult GetA()
+        {
+            return Ok(HttpContext.Current.Session["Some"] as SessionModel);
         }
     }
 }
