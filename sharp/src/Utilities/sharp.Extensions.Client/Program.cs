05b98c9c3fa42e831735836cb62caa26129e8220
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-10-05 17:45:16 +0400

    react added

--- sharp/src/Utilities/sharp.Extensions.Client/Program.cs
+++ sharp/src/Utilities/sharp.Extensions.Client/Program.cs
@@ -1,10 +1,23 @@
 ﻿using sharp.Extensions.Common;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace sharp.Extensions.Client
 {
     class Program
     {
+        public async Task CompleteWork()
+        {
+            await Task.WhenAll(Task.Run(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Thread.Sleep(1000);
+                }
+            }));
+        }
+
         [STAThread]
         private static void Main()
         {
