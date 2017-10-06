b951ffec3c28150114fb8c3adb55cf31274c6031
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-10-06 18:07:33 +0400

    ASP.NET core web api sample added to the repository

---
+++ sharp.netCore/ASP.NET/sharp.AspNet.WebApi/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sharp.AspNet.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Default")]
+    public class DefaultController : Controller
+    {
+        [HttpGet, Route("")]
+        public IActionResult Get()
+        {
+            var headers = this.HttpContext.Request.Headers;
+            return Ok(new[] { "value1", "value2" });
+        }
+    }
+}
