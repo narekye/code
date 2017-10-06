14bf348b452b67cf3a584e79458afaf5df170549
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-10-06 10:47:50 +0400

    if check removed

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -40,7 +40,7 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        if (calculateWinner(squares)) {
+        if (calculateWinner(squares) || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -49,6 +49,7 @@ class Board extends React.Component {
             xIsNext: !this.state.xIsNext
         });
     }
+    
     render() {
         const winner = calculateWinner(this.state.squares);
         let status;
