39a5ed0b78c9d485dc52f38de7eebd46cbb4892b
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-10-18 16:02:11 +0400

    changed

--- sharp/src/Web/sharp.aspnet.webapi/Core/BaseApiController.cs
+++ sharp/src/Web/sharp.aspnet.webapi/Core/BaseApiController.cs
@@ -9,6 +9,7 @@ namespace sharp.aspnet.webapi.Core
     public class BaseApiController : ApiController
     {
         public string Session { get; set; } // You can change type of session
+        [NonAction]
         protected new virtual HttpResponseMessage Ok<T>(T data = null) where T : class
         {
             HttpResponseMessage message;
@@ -21,37 +22,37 @@ namespace sharp.aspnet.webapi.Core
             message = Request.CreateResponse(HttpStatusCode.OK, response);
             return message;
         }
-
+        [NonAction]
         public virtual HttpResponseMessage Created()
         {
             return Request.CreateResponse(HttpStatusCode.Created);
         }
-
+        [NonAction]
         public virtual HttpResponseMessage Accepted()
         {
             return Request.CreateResponse(HttpStatusCode.Accepted);
         }
-
+        [NonAction]
         public new virtual HttpResponseMessage BadRequest()
         {
             return Request.CreateResponse(HttpStatusCode.BadRequest);
         }
-
+        [NonAction]
         public new virtual HttpResponseMessage NotFound()
         {
             return Request.CreateResponse(HttpStatusCode.NotFound);
         }
-
+        [NonAction]
         public virtual HttpResponseMessage BadGateway()
         {
             return Request.CreateResponse(HttpStatusCode.BadGateway);
         }
-
+        [NonAction]
         public virtual HttpResponseMessage Unauthorized()
         {
             return Request.CreateResponse(HttpStatusCode.Unauthorized);
         }
-
+        [NonAction]
         public virtual HttpResponseMessage CreateHtmlResponse(string html)
         {
             var response = new HttpResponseMessage();
