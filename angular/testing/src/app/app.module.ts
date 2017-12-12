2f9765cea7da26d0d5a92681cececd11ae1e03de
Narek Yegoryan <yegoryan.narek@gmail.com>
2017-12-12 10:32:39 +0400

    course project part 3 complete (directives)

--- angular/testing/src/app/app.module.ts
+++ angular/testing/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DropdownDirective } from './shared/dropdown.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
@@ -18,7 +19,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
   BrowserModule
