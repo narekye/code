db3e12226b6abefdd711376c04e5a92a0a89627b
Narek Yegoryan <narek.yegoryan@F3BO-01041>
2018-01-31 12:29:36 +0400

    com project

---
+++ COM/Database_COMObject/Interfaces/DBCOM_Events.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Database_COMObject.Interfaces
+{
+    [Guid("47C976E0-C208-4740-AC42-41212D3C34F0")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface DBCOM_Events
+    {
+    }
+}
---
+++ COM/Database_COMObject/Interfaces/DBCOM_Interface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Database_COMObject.Interfaces
+{
+    [Guid("694C1820-04B6-4988-928F-FD858B95C880")]
+    public interface DBCOM_Interface
+    {
+        [DispId(1)]
+        void Init(string userID, string password);
+        [DispId(2)]
+        bool ExecuteSelectCommand(string selCommand);
+        [DispId(3)]
+        bool NextRow();
+        [DispId(4)]
+        void ExecuteNonSelectCommand(string insCommand);
+        [DispId(5)]
+        string GetColumnData(int pos);
+    }
+}
