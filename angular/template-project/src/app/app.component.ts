59f0919dbfa97e8a8bfea870355e0c24349c87b9
Narek Yegoryan <narek.yegoryan@F3BO-01041>
2018-04-13 17:28:04 +0400

    Corrections to imported modules

--- angular/template-project/src/app/app.component.ts
+++ angular/template-project/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpModule, Http } from '@angular/http';
 import { Component } from "@angular/core";
-import { HttpClient } from '@angular/common/http'
 import { Response } from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
     selector: 'app-root',
@@ -10,7 +10,9 @@ import { Response } from '@angular/http';
 })
 
 export class AppComponent {
-    constructor(private http: HttpClient) {
-        this.http.get('https://jsonplaceholder.typicode.com/posts/1').subscribe((response: Response) => { });
+    constructor(private http: Http) {
+        this.http.get('https://jsonplaceholder.typicode.com/posts/1').map((response: Response) => {
+            return ""; 
+        }).subscribe((result: string) /* depends on the type of returning map function */ => { });
     }
 }
--- angular/template-project/src/app/app.module.ts
+++ angular/template-project/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppComponent } from "./app.component";
         BrowserModule,
         AppRoutingModule,
         CoreModule,
-        HttpClientModule
+        HttpModule
     ],
     providers: [
     ],
