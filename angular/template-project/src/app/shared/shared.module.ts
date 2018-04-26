579c0264678302a9632dc319afa9f1bd4efe2286
Narek Yegoryan <narek.yegoryan@F3BO-01041>
2018-04-26 16:27:47 +0400

    Shared module refactoring

--- angular/template-project/src/app/shared/shared.module.ts
+++ angular/template-project/src/app/shared/shared.module.ts
@@ -1,10 +1,7 @@
-import { BaseService } from './base-service';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [HttpClientModule],
-    providers: [BaseService]
+    
 })
 export class SharedModule {
 
