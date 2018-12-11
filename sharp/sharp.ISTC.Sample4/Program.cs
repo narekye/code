84e53d4b785e04f7fc7f2a6c1ede993cee02604b
narekye <narek.yegoryan@alenasolutions.com>
2018-12-11 10:59:06 +0400

    sample 4

---
+++ sharp/sharp.ISTC.Sample4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp.ISTC.Sample4
+{
+    class A
+    {
+        public virtual void DoWork()
+        {
+            Console.WriteLine($"{nameof(A)} does work!");
+        }
+    }
+
+    class B : A
+    {
+        public override void DoWork()
+        {
+            Console.WriteLine($"{nameof(B)} does work!");
+        }
+    }
+
+    class C : B
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A variable = new B();
+
+            variable.DoWork();
+
+            variable = new C();
+
+            variable.DoWork();
+
+            Console.Read();
+        }
+    }
+}
